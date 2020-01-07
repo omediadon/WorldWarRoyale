747606f46f9b4b79ea1427452019d3f101711e66
SAKHRAOUI Omar <15385929+omediadon@users.noreply.github.com>
2020-01-07 22:30:08 +0100

    spawn points

---
+++ TPS/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerHealth : Distructible {
+	[SerializeField] SpawnPoint[] spawnPoints;
+
+	public override void Die() {
+		base.Die();
+		spawnAtNew();
+	}
+
+	void spawnAtNew() {
+		int spawnIdx = Random.Range(0, spawnPoints.Length);
+
+		transform.position = spawnPoints[spawnIdx].transform.position;
+		transform.rotation = spawnPoints[spawnIdx].transform.rotation;
+	}
+
+	[ContextMenu("die")]
+	void testDie() {
+		Die();
+	}
+}
